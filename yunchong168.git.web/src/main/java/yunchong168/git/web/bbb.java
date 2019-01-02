d05a10005d76ffbe5d64764aa0f0dddb9600ff62
steven <steven@DESKTOP-ER1O460>
2019-01-02 15:04:51 +0800

    444444

--- yunchong168.git.web/src/main/java/yunchong168/git/web/bbb.java
+++ yunchong168.git.web/src/main/java/yunchong168/git/web/bbb.java
@@ -1,5 +1,13 @@
 package yunchong168.git.web;
 
 public class bbb {
+  private String image;
 
+public String getImage() {
+    return image;
+}
+
+public void setImage(String image) {
+    this.image = image;
+}
 }
